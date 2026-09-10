b3fff5d933b40166fdda512dda83bedba7a1bee3
nilberm <nilber16@outlook.com>
2026-09-10 14:35:34 -0300

    feat: week 2 learning activity

---
+++ scripts/prophets.js
@@ -0,0 +1,38 @@
+const url = 'https://byui-cse.github.io/cse-ww-program/data/latter-day-prophets.json';
+
+const cards = document.querySelector('#cards');
+
+async function getProphetData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    // console.table(data.prophets);
+    displayProphets(data.prophets);
+}
+
+const displayProphets = (prophets) => {
+    prophets.forEach((prophet) => {
+        const card = document.createElement('section');
+        const fullName = document.createElement('h2');
+        const portrait = document.createElement('img');
+        const details = document.createElement('p');
+
+        fullName.textContent = `${prophet.name} ${prophet.lastname}`;
+
+        portrait.setAttribute('src', prophet.imageurl);
+        portrait.setAttribute('alt', `Portrait of ${prophet.name} ${prophet.lastname}`);
+        portrait.setAttribute('loading', 'lazy');
+        portrait.setAttribute('width', '340');
+        portrait.setAttribute('height', '440');
+
+        details.innerHTML = `<span><strong>Date of Birth:</strong> ${prophet.birthdate}</span>
+            <span><strong>Place of Birth:</strong> ${prophet.birthplace}</span>`;
+
+        card.appendChild(fullName);
+        card.appendChild(portrait);
+        card.appendChild(details);
+
+        cards.appendChild(card);
+    });
+};
+
+getProphetData();
